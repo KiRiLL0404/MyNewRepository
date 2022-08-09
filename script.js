1683ee0f18fac936432af4d8f799a9a410c33253
KiRiLL0404 <kir042002@mai.ru>
2022-08-10 02:16:14 +0700

    Исправил ошибку с разбиванием строки на массив

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ console.log(screens.length);
 
 console.log("Стоимость верстки экранов " + screenPrice + " рублей/" + screenPrice * 0.017 + " долларов/" + screenPrice * 0.61 + " гривен/" + screenPrice * 0.112 + " юани");
 console.log("Стоимость разработки сайта " + fullPrice + " рублей/" + fullPrice * 0.017 + " долларов/" + fullPrice * 0.61 + " гривен/" + fullPrice * 0.112 + " юани");
-console.log((screens.toLowerCase()).split());
+console.log((screens.toLowerCase()).split(", "));
 console.log(fullPrice * (rollback / 100));
 //Пожалуйста пустите на 3ти урок((
