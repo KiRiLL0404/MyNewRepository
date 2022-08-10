8d1f53299f163fcdbc8ad06d3f016ea446212f7f
KiRiLL0404 <kir042002@mai.ru>
2022-08-11 04:12:19 +0700

    Выполнил задание номер 3

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let title = "PROJECT ON CURS";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 5000;
-let rollback = Math.round(Math.random() * 100);
-let fullPrice = 10000;
-let adaptive = true;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let rollback = Math.random();
+let adaptive = !!prompt("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice + (fullPrice * rollback));
 
-console.log(typeof title);
+/*console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
 console.log(screens.length);
 
 console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+
 console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 console.log((screens.toLowerCase()).split(", "));
-console.log(fullPrice * (rollback / 100));
-//Пожалуйста пустите на 3ти урок((
+*/
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) console.log("Даем скидку в 10%");
+else if (fullPrice >= 15000) console.log("Даем скидку в 5%");
+else if (fullPrice >= 0) console.log("Скидка не предусмотрена");
+else console.log("Что то пошло не так");
+
+
